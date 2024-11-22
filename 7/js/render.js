25e2b9703920aaf32f8f402ac4a5895502fed991
Keks <keks@htmlacademy.ru>
2024-11-22 10:13:20 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/render.js
+++ 7/js/render.js
@@ -1,24 +1,20 @@
-import { generatePhotos } from './data.js';
-
 const render = (photos) => {
   const picturesContainer = document.querySelector('.pictures');
   const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
   const fragment = document.createDocumentFragment();
 
-  photos.forEach(({ url, description, likes, comments }) => {
-    const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__img').src = url;
-    pictureElement.querySelector('.picture__img').alt = description;
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
-    fragment.appendChild(pictureElement);
-  });
+  if (photos) {
+    photos.forEach(({ url, description, likes, comments }) => {
+      const pictureElement = pictureTemplate.cloneNode(true);
+      pictureElement.querySelector('.picture__img').src = url;
+      pictureElement.querySelector('.picture__img').alt = description;
+      pictureElement.querySelector('.picture__likes').textContent = likes;
+      pictureElement.querySelector('.picture__comments').textContent = comments.length;
+      fragment.appendChild(pictureElement);
+    });
 
-  picturesContainer.appendChild(fragment);
+    picturesContainer.appendChild(fragment);
+  }
 };
-
-const photos = generatePhotos();
-
-render(photos);
 
 export { render };
